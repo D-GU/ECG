2ded42dabebdd15b99fc24ca29c15654598f06b9
forest <dgodomg@gmail.com>
2022-07-01 19:16:42 +0300

    01.07

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 import neurokit2 as nk
 import matplotlib.pyplot as plt
 
+_CHANEL = 12
+
 
 # Reading data from np.array file
 def _get_raw_data(_path: str):
@@ -10,22 +12,21 @@ def _get_raw_data(_path: str):
 
 
 # Showing graphs of ECG(_plot1 - RAW file data, _plot2 - gen data)
-def show_plot(_plot1: np.array, _plot2: np.array, _title: str) -> None:
-    chanel = 12
+def show_plot(_plot1: np.array, _title: str) -> None:
     plt.title(_title)
     plt.rcParams['figure.figsize'] = [20, 5]
     plt.rcParams['font.size'] = 14
 
-    plt.plot(_plot1[0][0::, chanel - 1], 'b-', label='Raw data')
-    plt.plot(_plot2, 'r-', label='Generated data')
-
+    plt.plot(_plot1, 'b-', label='Raw data')
     plt.show()
+
     return None
 
 
-_ecg_raw_data = _get_raw_data("/home/forest/Developer/Python Development/ECG/ecg_ptbxl.npy")
-ecg12 = nk.ecg_simulate(duration=10, sampling_rate=100)
+# Get raw data
+_ecg_raw_data = _get_raw_data("ecg_ptbxl.npy")
 
-signals, info = nk.ecg_process(ecg12, sampling_rate=100)
+# Get pre-processed data from one of the signals
+signals, info = nk.ecg_process(_ecg_raw_data[0][0:, _CHANEL - 1], sampling_rate=100)
 
-show_plot(_ecg_raw_data, ecg12, "Plots")
+df = pd.DataFrame(signals)
