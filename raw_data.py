788c348d7d8c15f36d8b0215ba84b2251008a948
D-GU <dgodomg@gmail.com>
2022-09-05 16:48:37 +0300

    ***

--- raw_data.py
+++ raw_data.py
@@ -1,11 +1,6 @@
-from pandas import DataFrame
 from numpy import load
 
 
 # Reading data from file
 def get_raw_data(_path: str):
     return load(_path)
-
-
-# Get raw data
-ecg_raw_data = get_raw_data("ecg_ptbxl.npy")
