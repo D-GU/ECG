ac4c3c0bd99f0e4c2174424f0caf185398b89d12
D-GU <dgodomg@gmail.com>
2022-11-27 15:42:53 +0300

    */

--- visualizer.py
+++ visualizer.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
+from neurokit2 import ecg_plot
 from functions import *
 from processes import *
 from raw_data import get_raw_data
@@ -11,6 +12,14 @@ data = get_raw_data("train.npy")
 sampling_rate = 100  # Sampling rate
 
 
-def get_parameters(_signal: np.array, num_sample: int, _lead: int, _sampling_rate: int):
-    _preprocessed, info = preprocess(_signal[num_sample][_lead], _sampling_rate)
+def get_plots():
+    return 0
 
+
+def get_parameters(_signal: np.array, _leads: int, _sampling_rate: int):
+    _preprocessed, info = preprocess(_signal, _sampling_rate)
+    plt.plot(_preprocessed["ECG_Raw"])
+    plt.show()
+
+
+get_parameters(data[0][:, 0], 1, 100)
